2783191fa2bec4513633f569c1b5602818685096
gali4 <gali3@uh.edu>
2018-10-11 16:48:01 -0500

    some basic settings

--- src/reducers/reducer_books.js
+++ src/reducers/reducer_books.js
@@ -1,8 +1,16 @@
+// export default function() {
+//   return [
+//     { title: "Javascript: The Good Parts", pages: 101 },
+//     { title: "Harry Potter", pages: 39 },
+//     { title: "The Dark Tower", pages: 85 },
+//     { title: "Eloquent Ruby", pages: 1 }
+//   ];
+// }
 export default function() {
   return [
-    { title: "Javascript: The Good Parts", pages: 101 },
+    { title: "Javascript: The Good Parts" },
     { title: "Harry Potter", pages: 39 },
     { title: "The Dark Tower", pages: 85 },
     { title: "Eloquent Ruby", pages: 1 }
-  ];
+  ]
 }
